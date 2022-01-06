f47dcba7b1d7cab13825af9ec9b288e0a5f3bec7
Sindi Tasellari <91495596+sinditasellari@users.noreply.github.com>
2022-01-06 01:27:21 +0100

    Add files via upload

---
+++ Assignment 10/a10_p5/critter.cpp
@@ -0,0 +1,66 @@
+/*
+   CH-230-A
+   a10.p5.cpp
+   Sindi Tasellari
+   stasellari@jacobs-university.de
+*/
+ 
+#include <iostream>
+#include "Critter.h"
+ 
+using namespace std;
+
+double Critter::convert1(int x)
+{
+        return double(x)/10;
+}
+
+int Critter::convert2(double x)
+{
+        return x*10;
+}
+ 
+// The constructors
+Critter::Critter()
+{
+        cout << "First constructor called (empty)" <<endl;
+        name = "default_critter";
+        hunger = 0;
+        boredom = 0;
+        height = 5;
+}
+ 
+Critter::Critter(string name1)
+{
+        cout << "Second constructor called (name specified)" << endl;
+        name = name1;
+        hunger = 0;
+        boredom = 0;
+        height = 5;
+}
+ 
+Critter::Critter(string name1, int hunger1, int boredom1, double height1)
+{
+        cout << "Third constructor called (all parameters)" << endl;
+        name = name1;
+        hunger = convert1(hunger1);
+        boredom = boredom1;
+        height = height1;
+}
+ 
+void Critter::setHunger(int hunger1)
+{
+    hunger = convert1(hunger1);
+}
+ 
+double Critter::getHunger()
+{
+    return convert2(hunger);
+}
+ 
+/* Printing the results */
+void Critter::print_info()
+{
+    cout << "Your name: " << name << endl << "Your hunger: " << getHunger() << endl 
+    << "Your boredom: " << boredom << endl << "Your height: " << height << endl;
+}
